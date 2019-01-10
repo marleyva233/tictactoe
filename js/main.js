686a0d094909895f5eead251b5f38dbecfd4f97e
Maribel <marleyva233@gmail.com>
2019-01-10 07:40:28 -0800

    new js

--- js/main.js
+++ js/main.js
@@ -1,74 +1,32 @@
-$(document).ready(function(){
-	let player1= "X";
-	let player2= "O";
+var player="X";
 
-	let currentTurn=1;
-	let movesMade=0;
-
-	let square=$(".square");
-	let winnerContainer=$(".winner");
-	let reset=$(".reset")
-
-	square.on("click", function(e){
-		movesMade++;
-		if(currentTurn===1){
-			event.target.innerHTML=player1;
-			event.target.style.color="red";
-			event.target.style.backgroundColor="white";
-			currentTurn++;
+function playGame(cell_id){
+	var cell=document.getElementById(cell_id);
+	if(cell.innerHTML.trim()==""){
+		cell.innerHTML=player;
+		if(player=="X"){
+			player="O";
 		}else{
-			event.target.innerHTML=player2;
-			event.target.style.color="blue";
-			event.target.style.backgroundColor="white";
-			currentTurn--;
-
-			if (checkForWinner()){
-				let theWinner=currentTurn===1 ? player2: player1;
-				declareWinner();
-			}
-		};
-
-		reset.on('click', function(e){
-			let moves= Array.prototype.slice.call($(".square"));
-			moves.map((m)=>{
-				m.innerHTML="";
-			});
-			winnerContainer.html("");
-			winnerContainer.css("display", "none");
-			currentTurn=1;
-			movesMade=0;
-		});
-		console.log(movesMade);
-		function checkForWinner(){
-			if (movesMade>4){
-				let moves=Array.prototype.slice.call($(".square"));
-				let results=moves.map(function(square){
-					return square.innerHTML;
-				});
-				let winningCombos=[
-					[0, 1, 2],
-					[3, 4, 5],
-					[6, 7, 8],
-					[0, 3, 6],
-					[1, 4, 7],
-					[2, 5, 8],
-					[0, 4, 8],
-					[2, 4, 6] 
-				];
-				return winningCombos.find(function(combo){
-					if (results[combo[0]] !== "" && results[combo[1]] !=="" && results[combo[2]] !== "" && results[combo[0]] === results[combo[1]] && results[combo[1]] === results[combo[2]]){
-						return true;
-					} else {
-						return false;
-					}
-				});
-			};
-		};
-		function declareWinner(winner){
-			winnerContainer.css("display", "block");
-			reset.css("display", "block");
-			winner=winner===player1 ? "M" : "T";
-			winnerContainer.html(winner + " Wins!");
+			player="X";
 		}
-	});
-});
+		checkWinner();
+	}
+	console.log(player);
+}
+
+var winningCombos=[
+// horizontal
+["sq1", "sq2", "sq3"],
+["sq4", "sq5", "sq6"],
+["sq7", "sq8", "sq9"],
+// vertical
+["sq1", "sq4", "sq7"],
+["sq2", "sq5", "sq8"],
+["sq3", "sq6", "sq9"],
+// diagonal
+["sq1", "sq5", "sq9"],
+["sq3", "sq5", "sq7"]
+];
+function checkWinner(){
+
+}
